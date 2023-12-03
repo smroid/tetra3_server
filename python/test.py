7fd12e6a8cf7d2a809d1524310c946c9589010b8
smroid <smr@dt3.org>
2023-12-02 23:12:56 -0800

    finish server; add test client

---
+++ python/test.py
@@ -0,0 +1,63 @@
+import argparse
+import grpc
+import logging
+import time
+
+import tetra3_pb2
+import tetra3_pb2_grpc
+
+
+def main():
+    ap = argparse.ArgumentParser(description='Test gRPC client exercise plate-solver')
+    ap.add_argument('-p', '--port', type=int, default=50051, help='port to connect to')
+    args = ap.parse_args()
+
+    logger = logging.getLogger('test')
+    if not logger.hasHandlers():
+        logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s:%(name)s-%(levelname)s: %(message)s')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+    # y,x
+    centroids = [(616.895,  528.342), (433.709, 553.614), (581.358,  920.476),
+                 (682.187,  474.311), (493.610, 465.951), (301.412,  581.112),
+                 (125.019,  924.384), (736.228, 334.872), (734.000, 1001.650),
+                 (459.419,  324.615), (358.651, 459.774), ( 34.464,   95.481),
+                 (292.661,  714.715), ( 51.613, 853.937), (404.596,  400.519),
+                 ( 38.560, 1013.460), (126.645, 534.583), (671.500,  678.626),
+                 (147.357,   10.334), ( 32.093, 135.451), ( 35.331,  607.451),
+                 (268.499,  574.395), (501.656, 639.474), (495.555,  269.554),
+                 (555.537,  375.461), (628.456, 101.276)]
+    request = tetra3_pb2.SolveRequest()
+    for c in centroids:
+        image_coord = request.star_centroids.add()
+        image_coord.x = c[1]
+        image_coord.y = c[0]
+    request.image_width = 1024
+    request.image_height = 768
+    request.fov_estimate = 11
+    # request.match_max_error = 0.005
+
+    server_address = 'localhost:%d' % args.port
+    with grpc.insecure_channel(server_address) as channel:
+        stub = tetra3_pb2_grpc.Tetra3Stub(channel)
+
+        # Do a first call to warm up the connection; add timing around the second call..
+        discard = stub.SolveFromCentroids(request, timeout=10)
+
+        start = time.perf_counter()
+        response = stub.SolveFromCentroids(request, timeout=10)
+        elapsed = time.perf_counter() - start
+
+        print('Reponse: %s' % response)
+        solve_time = response.solve_time.seconds + response.solve_time.nanos / 1000000000
+        rpc_overhead = elapsed - solve_time
+        print('Time total=solve+RPC %.2f=%.2f+%.2f ms' %
+              (elapsed * 1000, solve_time * 1000, rpc_overhead * 1000))
+
+
+if __name__ == "__main__":
+    main()
--- python/tetra3_server.py
+++ python/tetra3_server.py
@@ -1,24 +1,26 @@
 import argparse
 from concurrent import futures
 import grpc
+import logging
+import time
 
-import tetra3_pb2 as pb2
-import tetra3_pb2_grpc as pb2_grpc
+import tetra3_pb2
+import tetra3_pb2_grpc
 
 import tetra3
 
 
-class Tetra3Servicer(pb2_grpc.Tetra3Servicer):
+class Tetra3Servicer(tetra3_pb2_grpc.Tetra3Servicer):
     def __init__(self, filename):
         self._tetra3 = tetra3.Tetra3(load_database=filename)
+        self._logger = logging.getLogger('tetra3.Tetra3')
 
     def SolveFromCentroids(self, request, context):
+        start = time.perf_counter()
         # Pick up the RPC deadline.
         time_remaining = context.time_remaining()
         if time_remaining is None:
             time_remaining = 1.0
-        print('time_remaining %s' % time_remaining)  # TEMPORARY
-        print('request %s' % request)  # TEMPORARY
 
         # Convert the request fields for call to Tetra3 solve_from_centroids()
         # function.
@@ -80,47 +82,57 @@ class Tetra3Servicer(pb2_grpc.Tetra3Servicer):
             return_matches=return_matches,
             return_visual=False,
             match_max_error=match_max_error)
-        print('Tetra3 result %s' % result_dict)  # TEMPORARY
 
-        # Convert to result proto.
-        result = pb2.SolveResult()
-        if result_dict['RA'] is not None:
-            result.image_center_coords.ra = result_dict['RA']
-        if result_dict['Dec'] is not None:
-            result.image_center_coords.dec = result_dict['Dec']
-        if result_dict['Roll'] is not None:
-            result.roll = result_dict['Roll']
-        if result_dict['FOV'] is not None:
-            result.fov = result_dict['FOV']
-        if result_dict['distortion'] is not None:
-            result.distortion = result_dict['distortion']
-        if result_dict['RMSE'] is not None:
-            result.rmse = result_dict['RMSE']
-        if result_dict['Matches'] is not None:
-            result.matches = result_dict['Matches']
-        if result_dict['Prob'] is not None:
-            result.prob = result_dict['Prob']
-        if result_dict['epoch_equinox'] is not None:
-            result.epoch_equinox = result_dict['epoch_equinox']
-        if result_dict['epoch_proper_motion'] is not None:
-            result.epoch_proper_motion = result_dict['epoch_proper_motion']
-        if result_dict['cache_hit_fraction'] is not None:
-            result.cache_hit_fraction = result_dict['cache_hit_fraction']
-        result.solve_time.seconds = result_dict['T_solve'] // 1000
-        result.solve_time.nanos = (result_dict['T_solve'] % 1000) * 1000000
-        if result_dict['RA_target'] is not None:
-            ra_list = result_dict['RA_target']
-            dec_list = result_dict['Dec_target']
+        result = tetra3_pb2.SolveResult()
+        # Populate result proto from Tetra3 result dict.
+        ra = result_dict.get('RA', None)
+        dec = result_dict.get('Dec', None)
+        roll = result_dict.get('Roll', None)
+        fov = result_dict.get('FOV', None)
+        distortion = result_dict.get('distortion', None)
+        rmse = result_dict.get('rmse', None)
+        matches = result_dict.get('Matches', None)
+        prob = result_dict.get('Prob', None)
+        epoch_equinox = result_dict.get('epoch_equinox', None)
+        epoch_proper_motion = result_dict.get('epoch_proper_motion', None)
+        cache_hit_fraction = result_dict.get('cache_hit_fraction', None)
+        ra_list = result_dict.get('RA_target', None)
+        dec_list = result_dict.get('Dec_target', None)
+        matched_stars_list = result_dict.get('matched_stars', None)
+        matched_centroids_list = result_dict.get('matched_centroids', None)
+        matched_cat_id_list = result_dict.get('matched_catID', None)
+
+        if ra is not None:
+            result.image_center_coords.ra = ra
+        if dec is not None:
+            result.image_center_coords.dec = dec
+        if roll is not None:
+            result.roll = roll
+        if fov is not None:
+            result.fov = fov
+        if distortion is not None:
+            result.distortion = distortion
+        if rmse is not None:
+            result.rmse = rmse
+        if matches is not None:
+            result.matches = matches
+        if prob is not None:
+            result.prob = prob
+        if epoch_equinox is not None:
+            result.epoch_equinox = epoch_equinox
+        if epoch_proper_motion is not None:
+            result.epoch_proper_motion = epoch_proper_motion
+        if cache_hit_fraction is not None:
+            result.cache_hit_fraction = cache_hit_fraction
+
+        if ra_list is not None:
             assert dec_list is not None
             assert len(ra_list) == len(dec_list)
             for i in range(len(ra_list)):
                 target_coord = result.target_coords.add()
                 target_coord.ra = ra_list[i]
                 target_coord.dec = dec_list[i]
-        if result_dict['matched_stars'] is not None:
-            matched_stars_list = result_dict['matched_stars']
-            matched_centroids_list = result_dict['matched_centroids']
-            matched_cat_id_list = result_dict['matched_catID']
+        if matched_stars_list is not None:
             assert matched_centroids_list is not None
             assert len(matched_stars_list) == len(matched_centroids_list)
             if matched_cat_id_list is not None:
@@ -135,11 +147,15 @@ class Tetra3Servicer(pb2_grpc.Tetra3Servicer):
                 if matched_cat_id_list is not None:
                     matched_star.cat_id = '%s' % matched_cat_id_list[i]
 
-        print('result %s' % result)  # TEMPORARY
+        elapsed = time.perf_counter() - start
+        result.solve_time.seconds = int(elapsed)
+        elapsed_frac = elapsed - int(elapsed)
+        result.solve_time.nanos = int(elapsed_frac * 1000000000)
+
         return result
 
 
-def serve():
+def startServer():
     ap = argparse.ArgumentParser(
         description='Runs gRPC server to plate-solve a list of star locations in ' \
         'an image to the celestial coordinates of the image.')
@@ -148,11 +164,11 @@ def serve():
     args = ap.parse_args()
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    pb2_grpc.add_Tetra3Servicer_to_server(Tetra3Servicer(args.filename), server)
+    tetra3_pb2_grpc.add_Tetra3Servicer_to_server(Tetra3Servicer(args.filename), server)
     server.add_insecure_port('[::]:%d' % args.port)
     server.start()
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    serve()
+    startServer()
